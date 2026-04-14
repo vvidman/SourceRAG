19979330ce1bc1e2d3c1316b8a301e7578b7f723
Viktor Vidman <vvidman@gmail.com>
2026-04-14 20:36:06 +0200

    Implement balzor Web UI based on SPEC-006-blazor-web-client.md

--- src/SourceRAG.Web/Program.cs
+++ src/SourceRAG.Web/Program.cs
@@ -1,27 +1,65 @@
+/*
+   Copyright 2026 Viktor Vidman (vvidman)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using SourceRAG.Web.Components;
+using SourceRAG.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddDownstreamApi("SourceRagApi", builder.Configuration.GetSection("SourceRagApi"))
+    .AddInMemoryTokenCaches();
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient<SourceRagApiClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["SourceRagApi:BaseUrl"]
+        ?? throw new InvalidOperationException("SourceRagApi:BaseUrl not configured"));
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.MapControllers();
 
 app.Run();
